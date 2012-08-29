779634356e4dde2145df29445f2f93e358797ce6
Andrew Lee Rubinger <alr@alrubinger.com>
2012-08-28 23:31:15 -0400

    [SHRINKRES-39] Infer "runtime" scope when "compile" is specified in ValidationUtil.fromDependencyTree

--- impl-maven-prototype/src/test/java/org/jboss/shrinkwrap/resolver/impl/maven/integration/ValidationUtil.java
+++ impl-maven-prototype/src/test/java/org/jboss/shrinkwrap/resolver/impl/maven/integration/ValidationUtil.java
@@ -61,10 +61,15 @@ public class ValidationUtil {
         return fromDependencyTree(dependencyTree, allowedScopes);
     }
 
-    public static ValidationUtil fromDependencyTree(File dependencyTree, List<String> allowedScopes)
+    public static ValidationUtil fromDependencyTree(final File dependencyTree, final List<String> allowedScopes)
         throws IllegalArgumentException {
 
         List<String> files = new ArrayList<String>();
+
+        // Adjust; if COMPILE scope is specified, then "runtime" is inferred
+        if (allowedScopes.contains("compile") && !allowedScopes.contains("runtime")) {
+            allowedScopes.add("runtime");
+        }
 
         BufferedReader input = null;
         try {
@@ -72,17 +77,22 @@ public class ValidationUtil {
 
             String line = null;
             while ((line = input.readLine()) != null) {
-                ArtifactHolder holder = new ArtifactHolder(line);
-                if (holder.root == true && !"jar".equals(holder.extension)) {
-                    // skip non-jar from dependency tree
-                    continue;
+                final ArtifactHolder holder = new ArtifactHolder(line);
+                if (holder.root == true) {
+                    if (!"jar".equals(holder.extension)) {
+                        // skip non-jar from dependency tree
+                        continue;
+                    }
+
+                    // Add, scope doesn't matter for the root
+                    files.add(holder.filename());
                 }
                 // add artifact if in allowed scope
-                else if (allowedScopes.isEmpty() || (!allowedScopes.isEmpty() && allowedScopes.contains(holder.scope))) {
+                else if (allowedScopes.isEmpty() || allowedScopes.contains(holder.scope)) {
                     files.add(holder.filename());
                 }
             }
-        } catch (IOException e) {
+        } catch (final IOException e) {
             throw new CoordinateParseException(MessageFormat.format(
                 "Unable to load dependency tree from {0} to verify", dependencyTree));
         } finally {
@@ -158,218 +168,223 @@ public class ValidationUtil {
         }
         Assert.fail(errorMessage.toString());
     }
-}
-
-/**
- * A holder for a line generated from Maven dependency tree plugin
- *
- * @author <a href="mailto:kpiwko@redhat.com">Karel Piwko</a>
- *
- */
-class ArtifactHolder {
-
-    final String groupId;
-    final String artifactId;
-    final String extension;
-    final String classifier;
-    final String version;
-    final String scope;
-
-    final boolean root;
 
     /**
-     * Creates an artifact holder from the input lien
+     * A holder for a line generated from Maven dependency tree plugin
      *
-     * @param dependencyCoords
-     */
-    ArtifactHolder(String dependencyCoords) {
-        int index = 0;
-        while (index < dependencyCoords.length()) {
-            char c = dependencyCoords.charAt(index);
-            if (c == '\\' || c == '|' || c == ' ' || c == '+' || c == '-') {
-                index++;
-            } else {
-                break;
-            }
-        }
-
-        for (int testIndex = index, i = 0; i < 4; i++) {
-            testIndex = dependencyCoords.substring(testIndex).indexOf(":");
-            if (testIndex == -1) {
-                throw new IllegalArgumentException("Invalid format of the dependency coordinates for "
-                    + dependencyCoords);
-            }
-        }
-
-        StringTokenizer st = new StringTokenizer(dependencyCoords.substring(index), ":");
-
-        this.groupId = st.nextToken();
-        this.artifactId = st.nextToken();
-        this.extension = st.nextToken();
-
-        // this is the root artifact
-        if (index == 0) {
-            this.root = true;
-
-            if (st.countTokens() == 1) {
-                this.classifier = "";
-                this.version = st.nextToken();
-            } else if (st.countTokens() == 2) {
-                this.classifier = st.nextToken();
-                this.version = st.nextToken();
-            } else {
-                throw new IllegalArgumentException("Invalid format of the dependency coordinates for "
-                    + dependencyCoords);
-            }
-
-            this.scope = "";
-        }
-        // otherwise
-        else {
-            this.root = false;
-
-            if (st.countTokens() == 2) {
-                this.classifier = "";
-                this.version = st.nextToken();
-                this.scope = extractScope(st.nextToken());
-            } else if (st.countTokens() == 3) {
-                this.classifier = st.nextToken();
-                this.version = st.nextToken();
-                this.scope = extractScope(st.nextToken());
-            } else {
-                throw new IllegalArgumentException("Invalid format of the dependency coordinates for "
-                    + dependencyCoords);
-            }
-        }
-    }
-
-    public String filename() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(artifactId).append("-").append(version);
-        if (classifier.length() != 0) {
-            sb.append("-").append(classifier);
-        }
-        sb.append(".").append(extension);
-
-        return sb.toString();
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-
-        sb.append("groupId=").append(groupId).append(", ");
-        sb.append("artifactId=").append(artifactId).append(", ");
-        sb.append("type=").append(extension).append(", ");
-        sb.append("version=").append(version);
-
-        if (scope != "") {
-            sb.append(", scope=").append(scope);
-        }
-
-        if (classifier != "") {
-            sb.append(", classifier=").append(classifier);
-        }
-
-        return sb.toString();
-    }
-
-    /*
-     * (non-Javadoc)
+     * @author <a href="mailto:kpiwko@redhat.com">Karel Piwko</a>
      *
-     * @see java.lang.Object#hashCode()
      */
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((artifactId == null) ? 0 : artifactId.hashCode());
-        result = prime * result + ((classifier == null) ? 0 : classifier.hashCode());
-        result = prime * result + ((extension == null) ? 0 : extension.hashCode());
-        result = prime * result + ((groupId == null) ? 0 : groupId.hashCode());
-        result = prime * result + (root ? 1231 : 1237);
-        result = prime * result + ((scope == null) ? 0 : scope.hashCode());
-        result = prime * result + ((version == null) ? 0 : version.hashCode());
-        return result;
-    }
+    private static class ArtifactHolder {
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see java.lang.Object#equals(java.lang.Object)
-     */
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
+        private static final String SCOPE_ROOT = "";
+
+        final String groupId;
+        final String artifactId;
+        final String extension;
+        final String classifier;
+        final String version;
+        final String scope;
+
+        final boolean root;
+
+        /**
+         * Creates an artifact holder from the input lien
+         *
+         * @param dependencyCoords
+         */
+        ArtifactHolder(String dependencyCoords) {
+
+            int index = 0;
+            while (index < dependencyCoords.length()) {
+                char c = dependencyCoords.charAt(index);
+                if (c == '\\' || c == '|' || c == ' ' || c == '+' || c == '-') {
+                    index++;
+                } else {
+                    break;
+                }
+            }
+
+            for (int testIndex = index, i = 0; i < 4; i++) {
+                testIndex = dependencyCoords.substring(testIndex).indexOf(":");
+                if (testIndex == -1) {
+                    throw new IllegalArgumentException("Invalid format of the dependency coordinates for "
+                        + dependencyCoords);
+                }
+            }
+
+            StringTokenizer st = new StringTokenizer(dependencyCoords.substring(index), ":");
+
+            this.groupId = st.nextToken();
+            this.artifactId = st.nextToken();
+            this.extension = st.nextToken();
+
+            // this is the root artifact
+            if (index == 0) {
+                this.root = true;
+
+                if (st.countTokens() == 1) {
+                    this.classifier = "";
+                    this.version = st.nextToken();
+                } else if (st.countTokens() == 2) {
+                    this.classifier = st.nextToken();
+                    this.version = st.nextToken();
+                } else {
+                    throw new IllegalArgumentException("Invalid format of the dependency coordinates for "
+                        + dependencyCoords);
+                }
+
+                this.scope = SCOPE_ROOT;
+            }
+            // otherwise
+            else {
+                this.root = false;
+
+                if (st.countTokens() == 2) {
+                    this.classifier = "";
+                    this.version = st.nextToken();
+                    this.scope = extractScope(st.nextToken());
+                } else if (st.countTokens() == 3) {
+                    this.classifier = st.nextToken();
+                    this.version = st.nextToken();
+                    this.scope = extractScope(st.nextToken());
+                } else {
+                    throw new IllegalArgumentException("Invalid format of the dependency coordinates for "
+                        + dependencyCoords);
+                }
+            }
+        }
+
+        public String filename() {
+            StringBuilder sb = new StringBuilder();
+            sb.append(artifactId).append("-").append(version);
+            if (classifier.length() != 0) {
+                sb.append("-").append(classifier);
+            }
+            sb.append(".").append(extension);
+
+            return sb.toString();
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+
+            sb.append("groupId=").append(groupId).append(", ");
+            sb.append("artifactId=").append(artifactId).append(", ");
+            sb.append("type=").append(extension).append(", ");
+            sb.append("version=").append(version);
+
+            if (scope != "") {
+                sb.append(", scope=").append(scope);
+            }
+
+            if (classifier != "") {
+                sb.append(", classifier=").append(classifier);
+            }
+
+            return sb.toString();
+        }
+
+        /*
+         * (non-Javadoc)
+         *
+         * @see java.lang.Object#hashCode()
+         */
+        @Override
+        public int hashCode() {
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + ((artifactId == null) ? 0 : artifactId.hashCode());
+            result = prime * result + ((classifier == null) ? 0 : classifier.hashCode());
+            result = prime * result + ((extension == null) ? 0 : extension.hashCode());
+            result = prime * result + ((groupId == null) ? 0 : groupId.hashCode());
+            result = prime * result + (root ? 1231 : 1237);
+            result = prime * result + ((scope == null) ? 0 : scope.hashCode());
+            result = prime * result + ((version == null) ? 0 : version.hashCode());
+            return result;
+        }
+
+        /*
+         * (non-Javadoc)
+         *
+         * @see java.lang.Object#equals(java.lang.Object)
+         */
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+            if (obj == null) {
+                return false;
+            }
+            if (getClass() != obj.getClass()) {
+                return false;
+            }
+            ArtifactHolder other = (ArtifactHolder) obj;
+            if (artifactId == null) {
+                if (other.artifactId != null) {
+                    return false;
+                }
+            } else if (!artifactId.equals(other.artifactId)) {
+                return false;
+            }
+            if (classifier == null) {
+                if (other.classifier != null) {
+                    return false;
+                }
+            } else if (!classifier.equals(other.classifier)) {
+                return false;
+            }
+            if (extension == null) {
+                if (other.extension != null) {
+                    return false;
+                }
+            } else if (!extension.equals(other.extension)) {
+                return false;
+            }
+            if (groupId == null) {
+                if (other.groupId != null) {
+                    return false;
+                }
+            } else if (!groupId.equals(other.groupId)) {
+                return false;
+            }
+            if (root != other.root) {
+                return false;
+            }
+            if (scope == null) {
+                if (other.scope != null) {
+                    return false;
+                }
+            } else if (!scope.equals(other.scope)) {
+                return false;
+            }
+            if (version == null) {
+                if (other.version != null) {
+                    return false;
+                }
+            } else if (!version.equals(other.version)) {
+                return false;
+            }
             return true;
         }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        ArtifactHolder other = (ArtifactHolder) obj;
-        if (artifactId == null) {
-            if (other.artifactId != null) {
-                return false;
-            }
-        } else if (!artifactId.equals(other.artifactId)) {
-            return false;
-        }
-        if (classifier == null) {
-            if (other.classifier != null) {
-                return false;
-            }
-        } else if (!classifier.equals(other.classifier)) {
-            return false;
-        }
-        if (extension == null) {
-            if (other.extension != null) {
-                return false;
-            }
-        } else if (!extension.equals(other.extension)) {
-            return false;
-        }
-        if (groupId == null) {
-            if (other.groupId != null) {
-                return false;
-            }
-        } else if (!groupId.equals(other.groupId)) {
-            return false;
-        }
-        if (root != other.root) {
-            return false;
-        }
-        if (scope == null) {
-            if (other.scope != null) {
-                return false;
-            }
-        } else if (!scope.equals(other.scope)) {
-            return false;
-        }
-        if (version == null) {
-            if (other.version != null) {
-                return false;
-            }
-        } else if (!version.equals(other.version)) {
-            return false;
-        }
-        return true;
-    }
 
-    private String extractScope(String scope) {
-        int lparen = scope.indexOf("(");
-        int rparen = scope.indexOf(")");
-        int space = scope.indexOf(" ");
+        private String extractScope(String scope) {
+            int lparen = scope.indexOf("(");
+            int rparen = scope.indexOf(")");
+            int space = scope.indexOf(" ");
 
-        if (lparen == -1 && rparen == -1 && space == -1) {
-            return scope;
-        } else if (lparen != -1 && rparen != -1 && space != -1) {
-            return scope.substring(0, space);
+            if (lparen == -1 && rparen == -1 && space == -1) {
+                return scope;
+            } else if (lparen != -1 && rparen != -1 && space != -1) {
+                return scope.substring(0, space);
+            }
+
+            throw new IllegalArgumentException("Invalid format of the dependency coordinates for artifact scope: "
+                + scope);
+
         }
-
-        throw new IllegalArgumentException("Invalid format of the dependency coordinates for artifact scope: " + scope);
 
     }
 
