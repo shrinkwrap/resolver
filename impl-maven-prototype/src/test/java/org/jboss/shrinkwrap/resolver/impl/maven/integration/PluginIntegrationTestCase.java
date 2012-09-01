4eedecf8d8c66595a6ff9032dd6f3d6167df4d6e
Andrew Lee Rubinger <alr@alrubinger.com>
2012-09-01 02:23:51 -0400

    [SHRINKRES-39] Fix PluginIntegrationTestCase to reflect current POM dependencies

--- impl-maven-prototype/src/test/java/org/jboss/shrinkwrap/resolver/impl/maven/integration/PluginIntegrationTestCase.java
+++ impl-maven-prototype/src/test/java/org/jboss/shrinkwrap/resolver/impl/maven/integration/PluginIntegrationTestCase.java
@@ -16,9 +16,13 @@ import org.junit.Test;
  * This tests are expected to fail in IDE until integration with IDE is done.
  *
  * @author <a href="kpiwko@redhat.com">Karel Piwko</a>
- *
  */
 public class PluginIntegrationTestCase {
+
+    /*
+     * NOTE: This test will depend upon information declared in this project's POM. Changes to the POM will impact the
+     * test.
+     */
 
     @Test
     public void loadCurrentProject() {
@@ -38,9 +42,13 @@ public class PluginIntegrationTestCase {
     public void strictlyLoadTestDependencies() {
         ConfiguredResolveStage resolver = Resolvers.use(MavenResolverSystem.class).configureFromPlugin();
 
-        File[] files = resolver.importRuntimeAndTestDependencies(new NonTransitiveStrategy()).as(File.class);
-
-        new ValidationUtil("shrinkwrap-impl-base", "junit", "commons-codec", "jetty").validate(files);
+        final File[] files = resolver.importRuntimeDependencies(new NonTransitiveStrategy()).as(File.class);
+        new ValidationUtil("maven-settings-builder", "plexus-interpolation", "maven-settings", "aether-util",
+            "aether-spi", "maven-model-builder", "wagon-provider-api", "plexus-cipher", "maven-repository-metadata",
+            "shrinkwrap-resolver-api-maven-prototype", "maven-model", "jsoup", "sisu-inject-plexus",
+            "maven-aether-provider", "plexus-utils", "wagon-file", "aether-api", "aether-connector-wagon",
+            "plexus-classworlds", "wagon-http-lightweight", "plexus-component-annotations", "aether-impl")
+            .validate(files);
     }
 
 }
