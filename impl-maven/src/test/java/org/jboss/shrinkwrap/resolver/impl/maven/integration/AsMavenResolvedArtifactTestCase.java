28699514b1b136887506f8d37db1b59a15bf5534
Karel Piwko <kpiwko@redhat.com>
2012-12-20 14:47:29 +0100

    SHRINKRES-93 fixed AsMavenResolvedArtifactTestCase repository paths on Windows7

--- impl-maven/src/test/java/org/jboss/shrinkwrap/resolver/impl/maven/integration/AsMavenResolvedArtifactTestCase.java
+++ impl-maven/src/test/java/org/jboss/shrinkwrap/resolver/impl/maven/integration/AsMavenResolvedArtifactTestCase.java
@@ -12,15 +12,31 @@ import org.jboss.shrinkwrap.resolver.api.maven.coordinate.MavenCoordinate;
 import org.jboss.shrinkwrap.resolver.api.maven.coordinate.MavenCoordinates;
 import org.jboss.shrinkwrap.resolver.api.maven.coordinate.MavenDependencies;
 import org.jboss.shrinkwrap.resolver.api.maven.coordinate.MavenDependency;
+import org.jboss.shrinkwrap.resolver.impl.maven.bootstrap.MavenSettingsBuilder;
 import org.jboss.shrinkwrap.resolver.impl.maven.util.ValidationUtil;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
  * Tests as(ResolvedArtifactInfo) and asSingle(ResolvedArtifactInfo) methods.
  *
  * @author <a href="mailto:mmatloka@gmail.com">Michal Matloka</a>
+ * @author <a href="mailto:kpiwko@redhat.com">Karel Piwko</a>
  */
 public class AsMavenResolvedArtifactTestCase {
+
+    @BeforeClass
+    public static void setRemoteRepository() {
+        System.setProperty(MavenSettingsBuilder.ALT_USER_SETTINGS_XML_LOCATION, "target/settings/profiles/settings.xml");
+        System.setProperty(MavenSettingsBuilder.ALT_LOCAL_REPOSITORY_LOCATION, "target/the-other-repository");
+    }
+
+    @AfterClass
+    public static void clearRemoteRepository() {
+        System.clearProperty(MavenSettingsBuilder.ALT_USER_SETTINGS_XML_LOCATION);
+        System.clearProperty(MavenSettingsBuilder.ALT_LOCAL_REPOSITORY_LOCATION);
+    }
 
     /**
      * Tests asResolvedArtifact().
@@ -32,17 +48,17 @@ public class AsMavenResolvedArtifactTestCase {
         final String artifactCanonicalFormB = "org.jboss.shrinkwrap.test:test-deps-b:jar:1.0.0";
 
         final MavenDependency dependencyA = MavenDependencies.createDependency(artifactCanonicalFormA, ScopeType.TEST,
-            false);
+                false);
         final MavenDependency dependencyB = MavenDependencies.createDependency(artifactCanonicalFormB, ScopeType.TEST,
-            false);
+                false);
 
         final MavenCoordinate originalCoordinateA = MavenCoordinates.createCoordinate(artifactCanonicalFormA);
         final MavenCoordinate originalCoordinateB = MavenCoordinates.createCoordinate(artifactCanonicalFormB);
 
         // when
-        final MavenResolvedArtifact[] resolvedArtifactInfos = Maven.resolver()
-            .loadPomFromFile("target/poms/test-child.xml").addDependencies(dependencyA, dependencyB).resolve()
-            .withoutTransitivity().asResolvedArtifact();
+        final MavenResolvedArtifact[] resolvedArtifactInfos = Maven.resolver().addDependencies(dependencyA, dependencyB)
+                .resolve()
+                .withoutTransitivity().asResolvedArtifact();
 
         // then
         new ValidationUtil("test-deps-a-1.0.0.jar").validate(resolvedArtifactInfos[0].as(File.class));
@@ -73,7 +89,7 @@ public class AsMavenResolvedArtifactTestCase {
 
         // when
         final MavenResolvedArtifact resolvedArtifact = Maven.resolver().loadPomFromFile("target/poms/test-parent.xml")
-            .resolve(artifactCanonicalForm).withoutTransitivity().asSingleResolvedArtifact();
+                .resolve(artifactCanonicalForm).withoutTransitivity().asSingleResolvedArtifact();
 
         // then
         new ValidationUtil("test-deps-a-1.0.0.jar").validate(resolvedArtifact.as(File.class));
@@ -94,12 +110,12 @@ public class AsMavenResolvedArtifactTestCase {
         final String artifactCanonicalForm = "org.jboss.shrinkwrap.test:test-parent:pom:1.0.0";
 
         final MavenDependency dependency = MavenDependencies.createDependency(artifactCanonicalForm, ScopeType.TEST,
-            false);
+                false);
 
         // when
         final MavenResolvedArtifact[] resolvedArtifactInfos = Maven.resolver()
-            .loadPomFromFile("target/poms/test-parent.xml").addDependencies(dependency).resolve().withoutTransitivity()
-            .asResolvedArtifact();
+                .loadPomFromFile("target/poms/test-parent.xml").addDependencies(dependency).resolve().withoutTransitivity()
+                .asResolvedArtifact();
 
         // then
         assertEquals("resolved artifact infos list should be empty for pom dependency", 0, resolvedArtifactInfos.length);
@@ -119,12 +135,12 @@ public class AsMavenResolvedArtifactTestCase {
         final MavenCoordinate child2Coordinate = MavenCoordinates.createCoordinate(child2CanonicalForm);
 
         final MavenDependency dependency = MavenDependencies.createDependency(artifactCanonicalForm, ScopeType.TEST,
-            false);
+                false);
 
         // when
         final MavenResolvedArtifact[] resolvedArtifactInfos = Maven.resolver()
-            .loadPomFromFile("target/poms/test-dependency.xml").addDependencies(dependency).resolve()
-            .withoutTransitivity().asResolvedArtifact();
+                .loadPomFromFile("target/poms/test-dependency.xml").addDependencies(dependency).resolve()
+                .withoutTransitivity().asResolvedArtifact();
 
         // then
         assertEquals("MavenResolvedArtifact list should have one element", 1, resolvedArtifactInfos.length);
