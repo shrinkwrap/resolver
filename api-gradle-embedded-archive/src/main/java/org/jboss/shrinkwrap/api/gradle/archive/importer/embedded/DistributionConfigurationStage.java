48ce6d3fee59a83fe79f9dd8adbdbd980996e558
Michal Matloka <mmatloka@gmail.com>
2013-09-03 23:12:57 +0200

    SHRINKRES-124 Gradle embedded importer added

---
+++ api-gradle-embedded-archive/src/main/java/org/jboss/shrinkwrap/api/gradle/archive/importer/embedded/DistributionConfigurationStage.java
@@ -0,0 +1,57 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jboss.shrinkwrap.api.gradle.archive.importer.embedded;
+
+import java.io.File;
+import java.net.URI;
+
+/**
+ * @author <a href="mailto:mmatloka@gmail.com">Michal Matloka</a>
+ */
+public interface DistributionConfigurationStage extends ConfigurationStage {
+
+    /**
+     * Configures ShrinkWrap Resolver to build project with Gradle of given version.
+     *
+     * @param version
+     *            Version which will be used
+     */
+    ConfigurationStage useGradleVersion(final String version);
+
+    /**
+     * Use specified gradle distribution. It will be downloaded from given address.
+     *
+     * @param gradleDistribution
+     *            Gradle distribution which will be used
+     */
+    ConfigurationStage useDistribution(final URI gradleDistribution);
+
+    /**
+     * Use specified gradle installation.
+     *
+     * @param gradleHome
+     *            Gradle distribution which will be used
+     */
+    ConfigurationStage useInstallation(final File gradleHome);
+
+    /**
+     * Use default Gradle distribution.
+     */
+    ConfigurationStage useDefaultDistribution();
+
+}
---
+++ impl-gradle-embedded-archive/src/test/java/org/jboss/shrinkwrap/impl/gradle/archive/importer/embedded/WarEmbeddedGradleImporterTestCase.java
@@ -0,0 +1,46 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jboss.shrinkwrap.impl.gradle.archive.importer.embedded;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.gradle.archive.importer.embedded.EmbeddedGradleImporter;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+
+/**
+ * @author <a href="mailto:mmatloka@gmail.com">Michal Matloka</a>
+ */
+public class WarEmbeddedGradleImporterTestCase {
+
+    @Test
+    public void should() {
+        final String dir = "src/it/war-sample/";
+        final WebArchive webArchive = ShrinkWrap.create(EmbeddedGradleImporter.class).forProjectDirectory(dir)
+            .importBuildOutput().as(WebArchive.class);
+
+        AssertArchive.assertContains(webArchive, "WEB-INF/lib/commons-codec-1.7.jar");
+        AssertArchive.assertContains(webArchive, "WEB-INF/classes/test/nested/NestedWarClass.class");
+        AssertArchive.assertContains(webArchive, "WEB-INF/classes/test/WarClass.class");
+        AssertArchive.assertContains(webArchive, "WEB-INF/classes/main.properties");
+        AssertArchive.assertContains(webArchive, "WEB-INF/web.xml");
+        AssertArchive.assertNotContains(webArchive, "file.toExclude");
+        assertThat(webArchive.getContent().size()).isEqualTo(12);
+    }
+}
