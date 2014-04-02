ab4b4e64f6895a5ba42239da5f571fb61d394d8a
Karel Piwko <kpiwko@redhat.com>
2014-04-02 16:58:28 +0200

    SHRINKRES-175 Hardcoded ServiceLocator, JUL LoggingFactory

--- impl-maven/src/main/java/org/jboss/shrinkwrap/resolver/impl/maven/bootstrap/SecurityActions.java
+++ impl-maven/src/main/java/org/jboss/shrinkwrap/resolver/impl/maven/bootstrap/SecurityActions.java
@@ -16,6 +16,7 @@
  */
 package org.jboss.shrinkwrap.resolver.impl.maven.bootstrap;
 
+import java.lang.reflect.Constructor;
 import java.security.AccessController;
 import java.security.PrivilegedActionException;
 import java.security.PrivilegedExceptionAction;
@@ -139,6 +140,81 @@ final class SecurityActions {
                 throw (IllegalArgumentException) t;
             } else {
                 // No other checked Exception thrown by System.getProperty
+                try {
+                    throw (RuntimeException) t;
+                }
+                // Just in case we've really messed up
+                catch (final ClassCastException cce) {
+                    throw new RuntimeException("Obtained unchecked Exception; this code should never be reached", t);
+                }
+            }
+        }
+    }
+
+    /**
+     * Create a new instance by finding a constructor that matches the argumentTypes signature
+     * using the arguments for instantiation.
+     *
+     * @param className Full classname of class to create
+     * @param argumentTypes The constructor argument types
+     * @param arguments The constructor arguments
+     * @return a new instance
+     * @throws IllegalArgumentException if className, argumentTypes, or arguments are null
+     * @throws RuntimeException if any exceptions during creation
+     * @author <a href="mailto:aslak@conduct.no">Aslak Knutsen</a>
+     * @author <a href="mailto:andrew.rubinger@jboss.org">ALR</a>
+     */
+    static <T> T newInstance(final Class<T> implClass, final Class<?>[] argumentTypes, final Object[] arguments) {
+        if (implClass == null) {
+            throw new IllegalArgumentException("ImplClass must be specified");
+        }
+        if (argumentTypes == null) {
+            throw new IllegalArgumentException("ArgumentTypes must be specified. Use empty array if no arguments");
+        }
+        if (arguments == null) {
+            throw new IllegalArgumentException("Arguments must be specified. Use empty array if no arguments");
+        }
+        final T obj;
+
+        try {
+            Constructor<T> constructor = getConstructor(implClass, argumentTypes);
+            if (!constructor.isAccessible()) {
+                constructor.setAccessible(true);
+            }
+            obj = constructor.newInstance(arguments);
+        } catch (Exception e) {
+            throw new RuntimeException("Could not create new instance of " + implClass, e);
+        }
+
+        return obj;
+    }
+
+    /**
+     * Obtains the Constructor specified from the given Class and argument types
+     *
+     * @param clazz
+     * @param argumentTypes
+     * @return
+     * @throws NoSuchMethodException
+     */
+    static <T> Constructor<T> getConstructor(final Class<T> clazz, final Class<?>... argumentTypes)
+            throws NoSuchMethodException {
+        try {
+            return AccessController.doPrivileged(new PrivilegedExceptionAction<Constructor<T>>() {
+                public Constructor<T> run() throws NoSuchMethodException {
+                    return clazz.getDeclaredConstructor(argumentTypes);
+                }
+            });
+        }
+        // Unwrap
+        catch (final PrivilegedActionException pae) {
+            final Throwable t = pae.getCause();
+            // Rethrow
+            if (t instanceof NoSuchMethodException) {
+                throw (NoSuchMethodException) t;
+            }
+            else {
+                // No other checked Exception thrown by Class.getConstructor
                 try {
                     throw (RuntimeException) t;
                 }
