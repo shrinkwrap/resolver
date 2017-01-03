34584a054ac6287dda8a270382ae1669bd783d8b
Matous Jobanek <mjobanek@redhat.com>
2017-01-03 16:26:54 +0000

    SHRINKRES-261 When multiple settings.xml files are merged then all profiles activated by default are taken; modified testcase to verify that settings.xml files are used correctly

--- maven/impl-maven/src/main/java/org/jboss/shrinkwrap/resolver/impl/maven/internal/SettingsXmlProfileSelector.java
+++ maven/impl-maven/src/main/java/org/jboss/shrinkwrap/resolver/impl/maven/internal/SettingsXmlProfileSelector.java
@@ -61,11 +61,12 @@ public class SettingsXmlProfileSelector implements ProfileSelector {
             if (p.getId() != null && context.getActiveProfileIds().contains(id)
                 && !context.getInactiveProfileIds().contains(id)) {
                 activeProfiles.add(p);
+                continue;
             }
             if (p.getActivation() != null && p.getActivation().isActiveByDefault()
                 && !context.getInactiveProfileIds().contains(p.getId())) {
                 activeProfiles.add(p);
-                break;
+                continue;
             }
             for (ProfileActivator activator : activators) {
                 if (activator.isActive(p, context, problems)) {
