f04847288f077f396c2592c07c890f42a4b711c4
Florian Besser <flbe@zuehlke.com>
2017-01-12 21:51:20 +0100

    SHRINKRES-263: When resolving a .war dependency, search for output from maven war plugin, not just compiled classes; Check against artifact type property, not extension when determining whether we deal with a war or not.

--- maven/impl-maven/src/test/java/org/jboss/shrinkwrap/resolver/impl/maven/PackageDirectoriesWithClassesTestCase.java
+++ maven/impl-maven/src/test/java/org/jboss/shrinkwrap/resolver/impl/maven/PackageDirectoriesWithClassesTestCase.java
@@ -15,6 +15,9 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+
 /**
  * This test case simulates behavior of an IDE - in IDE an artifact, that is also another module loaded in the IDE,
  * is not fetched from local/remote repository, but is referenced to the location of the module's directory.
@@ -64,6 +67,39 @@ public class PackageDirectoriesWithClassesTestCase {
 
         //Check if non existing items are null !
         Assert.assertNull(outputZipFile.getEntry("a/non-exist" + File.separator));
+
+    }
+
+    /**
+     * Test special logic when resolving a war dependency.
+     */
+    @Test
+    public void packageWar() throws IOException {
+        File artifactFile = new File(
+                System.getProperty("user.dir") + "/target/repository/org/jboss/shrinkwrap/test/test-war-with-resources/1.0.0/pom.xml");
+
+        Artifact testPomArtifactMock = Mockito.mock(Artifact.class);
+        Mockito.when(testPomArtifactMock.getGroupId()).thenReturn("org.jboss.shrinkwrap.test");
+        Mockito.when(testPomArtifactMock.getArtifactId()).thenReturn("test-war");
+        Mockito.when(testPomArtifactMock.getExtension()).thenReturn("war");
+        Mockito.when(testPomArtifactMock.getClassifier()).thenReturn("");
+        Mockito.when(testPomArtifactMock.getVersion()).thenReturn("1.0.0");
+        Mockito.when(testPomArtifactMock.getFile()).thenReturn(artifactFile);
+        Mockito.when(testPomArtifactMock.getProperty(eq(ArtifactProperties.TYPE), anyString())).thenReturn("war");
+
+        ArtifactRequest artifactRequest = new ArtifactRequest();
+        artifactRequest.setDependencyNode(new DefaultDependencyNode(new Dependency(testPomArtifactMock, "compile")));
+        ArtifactResult mockedArtResult = new ArtifactResult(artifactRequest);
+        mockedArtResult.setArtifact(testPomArtifactMock);
+
+        MavenResolvedArtifact mavenResolvedArtifact = MavenResolvedArtifactImpl.fromArtifactResult(mockedArtResult);
+        ZipFile outputZipFile = new ZipFile(mavenResolvedArtifact.asFile());
+
+        //Check if the included files were taken from the "target/Artifact.artifactId-Artifact.version" directory
+        Assert.assertNotNull(outputZipFile.getEntry("special/a.file"));
+
+        //Check if the default "target/classes" directory was not included
+        Assert.assertNull(outputZipFile.getEntry("a/a.file"));
 
     }
 }
