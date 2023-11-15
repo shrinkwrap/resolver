af0fde843df52f4df8f59d81d2558378b6217112
Petr Beran <pberan@redhat.com>
2023-11-15 10:57:03 +0100

    [SHRINKRES-320] Replace redundant boolean expressions

--- maven/impl-maven/src/test/java/org/jboss/shrinkwrap/resolver/impl/maven/integration/ClassifiersTestCase.java
+++ maven/impl-maven/src/test/java/org/jboss/shrinkwrap/resolver/impl/maven/integration/ClassifiersTestCase.java
@@ -65,7 +65,7 @@ public class ClassifiersTestCase {
         }
         jarFile.close();
 
-        Assert.assertTrue("Tests-jar artifact was resolved", containsFieldClass == true);
+        Assert.assertTrue("Tests-jar artifact was resolved", containsFieldClass);
     }
 
     /**
@@ -98,7 +98,7 @@ public class ClassifiersTestCase {
         }
         jarFile.close();
 
-        Assert.assertTrue("Tests-jar artifact was resolved", containsFieldClass == true);
+        Assert.assertTrue("Tests-jar artifact was resolved", containsFieldClass);
     }
 
     // SHRINKRES-162
@@ -148,6 +148,6 @@ public class ClassifiersTestCase {
         }
         jarFile.close();
 
-        Assert.assertTrue("Tests-jar artifact was resolved", containsFieldClass == true);
+        Assert.assertTrue("Tests-jar artifact was resolved", containsFieldClass);
     }
 }
--- maven/impl-maven/src/test/java/org/jboss/shrinkwrap/resolver/impl/maven/integration/ClasspathWorkspaceReaderTestCase.java
+++ maven/impl-maven/src/test/java/org/jboss/shrinkwrap/resolver/impl/maven/integration/ClasspathWorkspaceReaderTestCase.java
@@ -106,7 +106,7 @@ public class ClasspathWorkspaceReaderTestCase {
         }
         jarFile.close();
 
-        Assert.assertTrue("Classpath resolver was able to get tests package", containsTestClass == true);
+        Assert.assertTrue("Classpath resolver was able to get tests package", containsTestClass);
 
     }
 
@@ -135,7 +135,7 @@ public class ClasspathWorkspaceReaderTestCase {
         }
         jarFile.close();
 
-        Assert.assertTrue("Classpath resolver was able to get tests package", containsTestClass == true);
+        Assert.assertTrue("Classpath resolver was able to get tests package", containsTestClass);
 
     }
 
@@ -191,7 +191,7 @@ public class ClasspathWorkspaceReaderTestCase {
         }
         jarFile.close();
 
-        Assert.assertTrue("Classpath resolver was able to get api package", containsTestClass == true);
+        Assert.assertTrue("Classpath resolver was able to get api package", containsTestClass);
 
     }
 
