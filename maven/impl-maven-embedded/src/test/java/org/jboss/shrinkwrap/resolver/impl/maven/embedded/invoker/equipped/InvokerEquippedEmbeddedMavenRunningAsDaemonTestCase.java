afe7b9725be9da592b4cac255507747589528e71
Petr Beran <pberan@redhat.com>
2023-11-20 14:26:42 +0100

    [SHRINKRES-329] Simplifying array creations

--- maven/impl-maven-embedded/src/test/java/org/jboss/shrinkwrap/resolver/impl/maven/embedded/invoker/equipped/InvokerEquippedEmbeddedMavenRunningAsDaemonTestCase.java
+++ maven/impl-maven-embedded/src/test/java/org/jboss/shrinkwrap/resolver/impl/maven/embedded/invoker/equipped/InvokerEquippedEmbeddedMavenRunningAsDaemonTestCase.java
@@ -42,7 +42,7 @@ public class InvokerEquippedEmbeddedMavenRunningAsDaemonTestCase {
         Invoker invoker = new DefaultInvoker();
 
         request.setPomFile(workDirRule.prepareProject(pathToJarSamplePom));
-        request.setGoals(Arrays.asList(new String[] { "clean", "package" }));
+        request.setGoals(Arrays.asList("clean", "package"));
 
         request.setProperties(getPropertiesWithSkipTests());
 
